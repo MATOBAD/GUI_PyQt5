ffa2710da7a56af996a3a35cb99e93320156a72b
MatobaSeiki <e1b15097@st.oit.ac.jp>
2018-12-17 23:25:06 +0900

    fix layout

--- gui.py
+++ gui.py
@@ -2,7 +2,8 @@
 import sys
 from PyQt5.QtCore import Qt
 from PyQt5.QtWidgets import QApplication, QWidget, QPushButton,\
-    QLabel, QGridLayout, QMainWindow, QLineEdit
+    QLabel, QGridLayout, QMainWindow, QLineEdit, QHBoxLayout,\
+    QVBoxLayout, QPlainTextEdit
 import sip
 
 
@@ -13,35 +14,45 @@ class MainWindow(QWidget):
         self.initUI()
 
     def initUI(self):
+        # 画面を表示させる場所の指定(x, y, 横幅, 高さ)
+        self.window = QWidget()
         self.setGeometry(300, 300, 250, 150)
+
+        # Windowのタイトル
         self.setWindowTitle('QCheckBox')
 
+        # Label
+        self.label = QLabel('input_text: ')
+        self.answer_label = QLabel('answer: ')
+
         # Edit
-        self.text = QLineEdit()
+        self.input_text_box = QPlainTextEdit()
+        self.textbox1 = QLineEdit()
 
         # Button
         self.button = QPushButton('OK', self)
         self.button_quit = QPushButton('quit', self)
-        self.label = QLabel('init_label')
+
         self.button.clicked.connect(self.button_click)
         self.button_quit.clicked.connect(self.quit_click)
 
         # レイアウト配置
         self.layout = QGridLayout()
-        self.layout.addWidget(self.button)
-        self.layout.addWidget(self.text)
-        self.layout.addWidget(self.button_quit)
-        self.layout.addWidget(self.label)
+        # レイアウトの位置を決める(widget, 開始する縦の位置, 開始する横の位置, 縦の大きさ, 横の大きさ)
+        self.layout.addWidget(self.label, 0, 0)
+        self.layout.addWidget(self.input_text_box, 0, 1, 20, 40)
+        self.layout.addWidget(self.button, 20, 0)
+        self.layout.addWidget(self.button_quit, 20, 1)
+        self.layout.addWidget(self.answer_label, 21, 0)
+        self.layout.addWidget(self.textbox1, 21, 1)
         self.setLayout(self.layout)
 
         self.show()
 
     def button_click(self):
         # buttonのclickでラベルをクリア
-        sent = self.text.text()
-        self.textbox1 = QLineEdit()
+        sent = self.input_text_box.toPlainText()
         self.textbox1.setText(sent)
-        self.layout.addWidget(self.textbox1)
 
     def quit_click(self):
         # アプリの終了
